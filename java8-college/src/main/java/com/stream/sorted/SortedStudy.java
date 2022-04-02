0832ad5c4ed95bc837d0dc540406472781953c93
alex <jiahangsocket@126.com>
2022-04-02 16:09:58 +0800

    * 多条件复杂排序

--- java8-college/src/main/java/com/stream/sorted/SortedStudy.java
+++ java8-college/src/main/java/com/stream/sorted/SortedStudy.java
@@ -3,6 +3,7 @@ package com.stream.sorted;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +19,28 @@ public class SortedStudy {
         persons.add(person1);
         persons.add(person2);
         persons.add(person3);
+    }
+
+
+    //TODO 自定义比较器
+    @Test
+    public void s(){
+        /**
+         * 定义比较器类 继承Comparator接口，然后重写compare（a，b） 方法，返回的结果是int类型;
+         *  0  代表 a==b
+         *  1  代表 a>b
+         * -1  代表 a<b
+         */
+        Comparator<Person> comparator = new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                //按照年龄从大到小排序
+                return o1.getId() - o2.getId();
+            }
+        };
+        System.out.println("[Before]"+persons);
+        persons.sort(comparator);
+        System.out.println("[After]"+persons);
     }
 
 
@@ -59,9 +82,9 @@ public class SortedStudy {
         System.out.println("[Before]" + persons);
         /**
          * 这段代码意思是：
-         * 将Person先按照id从小到大排序，
-         * 如果id相同，那么再按照name从小到大排序，
-         * 如果name也相同，那么再按照age从小到大排序。
+         * 将Person先按照id从大到小排序，
+         * 如果id相同，那么再按照name从大到小排序，
+         * 如果name也相同，那么再按照age从大到小排序。
          */
         final List<Person> collect = persons.stream()
                 .sorted(Comparator.comparing(Person::getId, Comparator.reverseOrder())
@@ -70,6 +93,18 @@ public class SortedStudy {
                 .peek(System.out::println)
                 .collect(Collectors.toList());
         System.out.println("[After]" + collect);
+    }
+
+    @Test
+    public void warning(){
+        //TODO 以下写法，相当于只按照年龄排序
+        System.out.println("[Before]"+persons);
+        final List<Person> collect = persons.stream()
+                .sorted(Comparator.comparing(Person::getId))//没有意义
+                .sorted(Comparator.comparing(Person::getName))//没有意义
+                .sorted(Comparator.comparing(Person::getAge))//因为这里又按照了年龄排序，所以上面两个的排序是没有意义的(浪费算力)；
+                .collect(Collectors.toList());
+        System.out.println("[After]"+collect);
     }
 
 }
